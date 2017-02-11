3d82270ab13ba39ce8f2570dd72edf1d9c4e4773
yonas samuel <yonastlolo@yahoo.com>
2017-02-10 23:48:37 -0500

    condition-if-else

---
+++ pacman-if-else.js
@@ -0,0 +1,18 @@
+var powerPelletEaten = false;
+var ghosts = 4;
+
+while (true) {
+  console.log('Ghosts remaining: ' + ghosts);
+
+  if (powerPelletEaten == false) {
+    console.log('Pac-Man eats the power pellet.');
+    powerPelletEaten = true;
+  } else if (ghosts > 0) {
+    console.log('Pac-Man eats a ghost.');
+    ghosts--;
+  } else {
+    break;
+  }
+}
+
+console.log('Pac-Man returns home after a long day in the maze.');
